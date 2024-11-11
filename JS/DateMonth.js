e06ff8b75cba9a2b9218fc33c114987b4595f017
RegnbueSno <133234120+RegnbueSno@users.noreply.github.com>
2024-11-11 21:03:59 +0100

    Add files via upload

--- JS/DateMonth.js
+++ JS/DateMonth.js
@@ -1,30 +1,33 @@
 var today = new Date();
 
 var mm = today.getMonth() + 1;
+var dd = today.getDate()
+
+if (dd < 10) {
+    dd = '0' + dd;
+}
 
 if (mm < 10) {
     mm = '0' + mm;
 }
 
+birthday = dd + '-' + mm;
+
 today = mm;
 var pic;
-
+console.log(birthday);
 window.onload = function () {
     const test = document.getElementById('credImag');
-    console.log(test);
     if (today == 10) {
         pic = "pictures/events/monthevents/Halloween.png";
-        console.log(pic);
         test.src = pic
     } else if (today == 12) {
         pic = "pictures/events/monthevents/Christmas.png";
-        console.log(pic);
+        test.src = pic
+    } else if (birthday == "01-03") {
+        pic = "pictures/events/birthday.png";
         test.src = pic
     }
+        
 
-}
-
-
-
-
-console.log(today);
+    }
