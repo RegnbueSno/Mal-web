b29bf996017a49f9fb2b8358ad897fdad8f6b27a
RegnbueSno <winterbrightwater8rgmail.com>
2026-07-14 23:48:01 +0200

    Changed FTV section to be for Groups as we hadd a need of a space for multiple groups Mal is partnered/Teamed up with

--- files/JS/Sidebar.js
+++ files/JS/Sidebar.js
@@ -1,15 +1,25 @@
 
 function toggleSidebar() {
     const title = document.getElementById("title");
-    title.classList.toggle("open");
+    if (title) {
+        title.classList.toggle("open");
+    }
     const logo = document.getElementById("malsLogo");
-    logo.classList.toggle("open");
+    if (logo) {
+        logo.classList.toggle("open");
+    }
     const sidebar = document.getElementById("my-sidebar");
-    sidebar.classList.toggle("open");
+    if (sidebar) {
+        sidebar.classList.toggle("open");
+    }
     const toggleButton = document.getElementById("toggle-button");
-    toggleButton.classList.toggle("open");
+    if (toggleButton) {
+        toggleButton.classList.toggle("open");
+    }
     const sidebarNav = document.getElementById("sidebar-nav");
-    sidebarNav.classList.toggle("open");
+    if (sidebarNav) {
+        sidebarNav.classList.toggle("open");
+    }
     // Changed to querySelectorAll (note the dot for the class name)
     const sidebarContent = document.querySelectorAll(".sidebar-choice");
     sidebarContent.forEach(choice => {
