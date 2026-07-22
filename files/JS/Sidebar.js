918a7b78a753ebe47b55f6c028c3160153d53998
RegnbueSno <winterbrightwater8rgmail.com>
2026-07-22 16:38:57 +0200

    Uppdated Mobile View

--- files/JS/Sidebar.js
+++ files/JS/Sidebar.js
@@ -1,29 +1,33 @@
-
+const classOpen = "open"
 function toggleSidebar() {
     const title = document.getElementById("title");
     if (title) {
-        title.classList.toggle("open");
+        title.classList.toggle(classOpen);
     }
     const logo = document.getElementById("malsLogo");
     if (logo) {
-        logo.classList.toggle("open");
+        logo.classList.toggle(classOpen);
     }
     const sidebar = document.getElementById("my-sidebar");
     if (sidebar) {
-        sidebar.classList.toggle("open");
+        sidebar.classList.toggle(classOpen);
     }
     const toggleButton = document.getElementById("toggle-button");
     if (toggleButton) {
-        toggleButton.classList.toggle("open");
+        toggleButton.classList.toggle(classOpen);
     }
     const sidebarNav = document.getElementById("sidebar-nav");
     if (sidebarNav) {
-        sidebarNav.classList.toggle("open");
+        sidebarNav.classList.toggle(classOpen);
+    }
+    const mobileSidebarToggle = document.getElementById("mobile-toggle-button")
+    if (mobileSidebarToggle) {
+        mobileSidebarToggle.classList.toggle(classOpen);
     }
     // Changed to querySelectorAll (note the dot for the class name)
     const sidebarContent = document.querySelectorAll(".sidebar-choice");
     sidebarContent.forEach(choice => {
-        choice.classList.toggle("open");
+        choice.classList.toggle(classOpen);
     });
     
 
